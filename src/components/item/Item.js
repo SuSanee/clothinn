56483e911b9d842695db6577d4d874022cfb4fff
Anshika Agarwal <anshikaagarwal766@gmail.com>
2025-04-19 00:13:54 +0530

    Added lazy load effect for images

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Item.css";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 function Item({ name, rating, price, saleDiscount, image, brand }) {
   return (
     <div className="item-card">
-      <img src={image} alt={"Item image"} width="100%"/>
+      <LazyLoadImage
+        alt="Item image"
+        src={image}
+        effect="blur"
+        placeholderSrc="https://via.placeholder.com/300x300"
+        width="100%"
+      />
       <div className="item-brand">{brand}</div>
       <div className="item-name">{name}</div>
       <div className="item-info">
