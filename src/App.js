6cc6c04b49e0b0b911c597ec1e1cd4030a57ed32
Anshika Agarwal <anshikaagarwal766@gmail.com>
2025-04-18 20:57:50 +0530

    Initial commit- added project files

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./App.css";
+import HomePage from "./components/home/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemDetail from "./components/itemDetail/ItemDetail";
+import Navbar from "./components/navbar/Navbar";
+import Cart from "./components/cart/Cart";
+import Orders from "./components/orders/Orders";
+import Checkout from "./components/checkout/Checkout";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <Navbar />
+        </div>
+        <Routes>
+          <Route path="/item/:id" element={<ItemDetail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
